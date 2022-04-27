0644c63ba36a3be4cfec9140d1764d935e0deb7e
Deepak78956 <dk7895625828@gmail.com>
2022-04-27 12:59:58 +0530

    got operations section working

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
+//Modal Window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const btnScrollTo = document.querySelector('.git btn--scroll-to');
-const section1 = document.getElementById('section--1');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -32,6 +30,38 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// btn scroll
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.getElementById('section--1');
+
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//nav scroll
+const navigation = document.querySelector('.nav__links');
+
+navigation.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    document
+      .querySelector(e.target.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
